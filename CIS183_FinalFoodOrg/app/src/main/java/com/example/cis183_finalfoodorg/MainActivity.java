481d16d376a74ef3908037b50691f009e7cc8b54
chaseothan <52476148+chaseothan@users.noreply.github.com>
2023-11-29 17:23:12 -0500

    Revert "Merge branch 'main' of https://github.com/chaseothan/CIS183FinalFoodOrg"
    This reverts commit debaab4c0288b085942123578a1bfc1fb2bf7e2d, reversing
    changes made to afab54fe9ab1426ba593a9f02a056e661c428756.

--- CIS183_FinalFoodOrg/app/src/main/java/com/example/cis183_finalfoodorg/MainActivity.java
+++ CIS183_FinalFoodOrg/app/src/main/java/com/example/cis183_finalfoodorg/MainActivity.java
@@ -6,14 +6,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn_j_main_totalList;
-    Button btn_j_main_placeList;
+    Button btn_j_main_login;
+    Button btn_j_main_addNewUser;
+    Button btn_j_main_editUser;
+    ListView lv_j_main_listOfUsers;
+    EditText et_j_main_username;
+    EditText et_j_main_password;
 
-    Intent TotalListIntent;
-    Intent PlaceListIntent;
+
+
+    Intent HomePageintent;
+    Intent NewUserPageIntent;
+    Intent EditUserIntent;
+
 
 
 
@@ -23,12 +33,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        btn_j_main_totalList = findViewById(R.id.btn_v_main_totalList);
-        btn_j_main_placeList = findViewById(R.id.btn_v_main_placeList);
+        //btn_j_main_totalList = findViewById(R.id.btn_v_main_editUser);
+        //btn_j_main_placeList = findViewById(R.id.btn_v_main_removeUser);
+        lv_j_main_listOfUsers = findViewById(R.id.lv_v_main_listofusers);
+
+        et_j_main_password = findViewById(R.id.et_v_main_password);
+        et_j_main_username = findViewById(R.id.et_v_main_username);
+
+        btn_j_main_addNewUser = findViewById(R.id.btn_v_main_addUser);
+        btn_j_main_editUser = findViewById(R.id.btn_v_main_edituser);
+        btn_j_main_login = findViewById(R.id.btn_v_main_login);
 
 
-        TotalListIntent = new Intent(MainActivity.this, TotalList.class);
-        PlaceListIntent = new Intent(MainActivity.this, PlaceList.class);
+        EditUserIntent = new Intent(MainActivity.this, EditUser.class);
+        NewUserPageIntent = new Intent(MainActivity.this, NewUserPage.class);
+        HomePageintent = new Intent(MainActivity.this, HomePage.class);
+
+
+        //TotalListIntent = new Intent(MainActivity.this, TotalList.class);
+        //PlaceListIntent = new Intent(MainActivity.this, PlaceList.class);
 
         ButtonClickEventHandler();
     }
@@ -36,24 +59,47 @@ public class MainActivity extends AppCompatActivity {
     public void ButtonClickEventHandler()
     {
 
-        btn_j_main_totalList.setOnClickListener(new View.OnClickListener() {
+        //btn_j_main_totalList.setOnClickListener(new View.OnClickListener() {
+           // @Override
+           // public void onClick(View view)
+           // {
+
+            //    startActivity(TotalListIntent);
+
+            //}
+        //});
+
+        //btn_j_main_placeList.setOnClickListener(new View.OnClickListener() {
+         //   @Override
+          //  public void onClick(View view)
+          //  {
+           //     startActivity(PlaceListIntent);
+          //  }
+       // });
+
+        btn_j_main_addNewUser.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view)
-            {
-
-                startActivity(TotalListIntent);
-
+            public void onClick(View v) {
+                startActivity(NewUserPageIntent);
             }
         });
-
-        btn_j_main_placeList.setOnClickListener(new View.OnClickListener() {
+        btn_j_main_editUser.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view)
-            {
-                startActivity(PlaceListIntent);
+            public void onClick(View v) {
+                startActivity(EditUserIntent);
             }
         });
+        btn_j_main_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                //later in the if statement check if the user name and password are valid
+                if ()
+                {
+                    startActivity(HomePageintent);
+                }
+            }
+        });
 
     }
 
