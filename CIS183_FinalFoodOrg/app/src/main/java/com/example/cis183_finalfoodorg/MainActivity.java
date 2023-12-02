72c8c4a7f9690eca8e72814d8b63c4e7143e39a7
chaseothan <52476148+chaseothan@users.noreply.github.com>
2023-12-02 17:34:23 -0500

    added a lot of the assets
    named a lot of the stuff in project

--- CIS183_FinalFoodOrg/app/src/main/java/com/example/cis183_finalfoodorg/MainActivity.java
+++ CIS183_FinalFoodOrg/app/src/main/java/com/example/cis183_finalfoodorg/MainActivity.java
@@ -3,13 +3,11 @@ package com.example.cis183_finalfoodorg;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
 
@@ -46,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
         et_j_main_username = findViewById(R.id.et_v_main_username);
 
         btn_j_main_addNewUser =(ImageView) findViewById(R.id.btn_v_main_addUser);
-        btn_j_main_editUser =(ImageView) findViewById(R.id.btn_v_main_edituser);
+        btn_j_main_editUser =(ImageView) findViewById(R.id.btn_v_main_removeUser);
         btn_j_main_login = findViewById(R.id.btn_v_main_login);
 
 
